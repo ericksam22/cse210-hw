0aeb2d5061e4d60cccc99714ff3f6c7902be18e8
ericksam22 <ericktambriz4@gmail.com>
2023-01-27 20:38:20 -0600

    new form

--- csharp-prep/Journal.cs
+++ csharp-prep/Journal.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
+using System.Threading. Tasks;
+using static System.Cosole;
+
 
 namespace JournalApp
 
@@ -11,7 +13,33 @@ namespace JournalApp
 
     {
         public void Run ()
-        
+        {
+           DisplayIntro();
+
+           DisplayOutro();
+        }
+        private void CreateJournalFile(){ }
+
+        private void DisplayIntro(){ 
+
+                ForegroundColor = ConsoleColor.Black;
+                BackgroundColor = ConsoleColor.White;
+                Clear();
+             WriteLine("Journal App");
+                ReadKey(true);
+        }
+
+        private void DisplayOutro () {
+
+              WriteLine("Hi! :D ");
+              ReadKey(true);
+         }
+
+        private void DisplayJournalContents (){ }
+
+        private void ClearFile () { }
+
+        private void AddEntry () { }
     }
-    
+     
 }
---
+++ csharp-prep/programjournal.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace Journal
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Journal myJournal = new Journal();
+            myJournal.Run();
+             
+        }
+    }
+
+}
